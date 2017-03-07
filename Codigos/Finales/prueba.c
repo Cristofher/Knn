d4c4bc1eb926e6721ed0de27251951ad1620332c
Cristofher <Cristofherrojas.cr@gmail.com>
2017-03-07 16:10:15 -0300

    Avance

--- Codigos/Finales/prueba.c
+++ Codigos/Finales/prueba.c
@@ -3,63 +3,87 @@
 
 int main()
 {
-    FILE *p, *q, *r, *fp;
-    char c;
-    char textoExtraido [50];
-    char file1[200], file2[200], file3[200];
-    char cadena[256];
-    printf("Escribe el nombre del primer archivo a unir: ");
-    scanf("%s", file1);
-    printf("Escribe el nombre del segundo archivo a unir: ");
-    scanf("%s", file2);
-    printf("Escribe el nombre del archivo resultado a crear: ");
-    scanf("%s", file3);
-    p=fopen(file1, "r+");
-    q=fopen(file2, "r+");
-    r=fopen(file3, "w");
-    while(!feof(p))
-    {
-        fscanf(p, "%c", &c);
-        fprintf(r, "%c", c);
-    }
-    fprintf(r, "\n");
-    
-    while(!feof(q))
-    {
-        fscanf(q, "%c", &c);
-        fprintf(r, "%c", c);
-    }
+	FILE *p, *q, *r, *fp;
+	char c;
+	char textoExtraido [1000];
+	char file1[200], file2[200], file3[200];
+	char cadena[1000];
+	char cad;
+	printf("Escribe el nombre del primer archivo a unir: ");
+	scanf("%s", file1);
+	printf("Escribe el nombre del segundo archivo a unir: ");
+	scanf("%s", file2);
+	printf("Escribe el nombre del archivo resultado a crear: ");
+	scanf("%s", file3);
+	p=fopen(file1, "r+");
+	q=fopen(file2, "r+");
+	r=fopen(file3, "w");
+	char palabra[1000] = "->Funcion<-";
 
-    rewind(q);
-    
-    if (fgets(textoExtraido, 256, q)!= NULL)
-    {
-        printf("%s",textoExtraido);
-    }
+	while(!feof(p))
+	{
+		if (fgets(cadena, 1000, p) != NULL)
+		{
+			if (strcmp(cadena,palabra)==10)
+			{
+				printf("Entro aca");
+				fprintf(r, "%s", cadena);
+				for (int i = 0; i < 1000; ++i)
+				{
+					fprintf(r, " ");
+				}
+				printf("encontre\n");  
+			}
+			else{
+				fprintf(r, "%s", cadena);
+			}
+		}
+	}
+	fprintf(r, "\n");
+	fprintf(r, "\n");
 
-    fclose(r);
-    fp=fopen(file3, "rw+");
-    char palabra[256] = "->Funcion<-";
-    rewind(fp);
+	while(!feof(q))
+	{
+		fscanf(q, "%c", &c);
+		fprintf(r, "%c", c);
+	}
 
-    fseek( fp, 1862, SEEK_SET );
-    fputs("\n", fp);
-    fputs("C Programming Language", fp);
+	rewind(q);
 
-    while(!feof(fp))
-    {
-     if (fgets(cadena, 256, fp) != NULL)
-     {
-        if (strcmp(cadena,palabra)==0)
-        {
-            printf("reemplazar\n");  
-            fprintf( fp,"%s",textoExtraido);
-        }
-    }
-}
+	if (fgets(textoExtraido, 256, q)!= NULL)
+	{
+		printf("%s",textoExtraido);
+	}
 
-fclose(p);
-fclose(q);
-fclose(fp);
-return 0; 
+	fclose(r);
+	fp=fopen(file3, "rw+");
+	rewind(fp);
+
+
+
+	while(!feof(fp))
+	{
+		if (fgets(cadena, 256, fp) != NULL)
+		{
+			if (strcmp(cadena,palabra)==0)
+			{
+				printf("reemplazar\n");  
+				fprintf( fp,"%s",textoExtraido);
+			}
+		}
+	}
+
+	fseek( fp, 1862, SEEK_SET );
+	int len = strlen(textoExtraido);
+	textoExtraido[len-1]=';';
+	printf("%d\n", len);
+	fprintf( fp,"%s",textoExtraido);
+
+	while (fgets(textoExtraido))
+
+
+	fclose(p);
+	fclose(q);
+	fclose(fp);
+	return 0; 
 }
